0aac285f6d136f01ff56d29c5d60ffa40004edb7
Dayne <daynesouza@hotmail.com.br>
2023-04-13 15:17:14 -0300

    Implementando a função AdicionarProduto no carrinho

--- 7-alura-store/src/common/AdicionarProduto/index.tsx
+++ 7-alura-store/src/common/AdicionarProduto/index.tsx
@@ -5,40 +5,41 @@ import { Icarrinho } from 'types/carrinho';
 
 
 
-export default function adicionarCarrinho(produtoCarrinho: Icarrinho){
+export default function adicionarCarrinho(produtoCarrinho: Icarrinho) {
+    const carrinho = useRecoilValue(listaDeComprasState);
+    const setCarrinho = useSetRecoilState(listaDeComprasState);
 
-    function atualizaCarrinho(){
-        const carrinho = useRecoilValue(listaDeComprasState);
-        const setCarrinho = useSetRecoilState(listaDeComprasState);
+    const saldo = useRecoilValue(saldoTotalState);
+    const setSaldo = useSetRecoilState(saldoTotalState);
 
-        const saldo = useRecoilValue(saldoTotalState);
-        const setSaldo = useSetRecoilState(saldoTotalState);
+    function atualizaCarrinho() {
 
-        if(verificaCarrinhoVazio()){
+
+        if (verificaCarrinhoVazio()) {
             setCarrinho([produtoCarrinho])
-            setSaldo(saldo+produtoCarrinho.price)
-        }else{
+            setSaldo(saldo + produtoCarrinho.price)
+        } else {
             atualizaQuantidadeItem(carrinho);
         }
 
     }
 
-    function atualizaQuantidadeItem( carrinho:Icarrinho[]){
+    function atualizaQuantidadeItem(carrinho: Icarrinho[]) {
         let carrinhoAtual: Icarrinho[];
         let quantidade;
-        
-        carrinho.map( (item) => {
-            if (item.id = produtoCarrinho.id){
+
+        carrinho.map((item) => {
+            if (item.id = produtoCarrinho.id) {
                 quantidade = item.quantidade + produtoCarrinho.quantidade
-                carrinhoAtual.push({...item, quantidade})
-            }else{
+                carrinhoAtual.push({ ...item, quantidade })
+            } else {
                 carrinho.push(item)
             }
         })
     }
 
-    function verificaCarrinhoVazio(){
-        if (carrinho.length === 0){
+    function verificaCarrinhoVazio() {
+        if (carrinho.length === 0) {
             return true
         }
     }
